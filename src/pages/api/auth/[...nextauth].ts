0ae642749f96e3b8da3c5372f9b9868086861159
augustojs1 <augustojsouza1@gmail.com>
2021-12-18 13:48:40 -0300

    feature: add faunadb handler

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from "faunadb";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -12,6 +15,18 @@ export default NextAuth({
         },
       },
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user.user;
+
+      try {
+        await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
