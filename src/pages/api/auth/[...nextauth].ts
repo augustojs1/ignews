f42b06a4904ec2ff15007f19e20862c04001d8dd
augustojs1 <augustojsouza1@gmail.com>
2021-12-20 20:43:12 -0300

    fix: verify duplicate user

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -21,10 +21,19 @@ export default NextAuth({
       const { email } = user.user;
 
       try {
-        await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(q.Match(q.Index("user_by_email"), q.Casefold(email)))
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(email)))
+          )
+        );
 
         return true;
-      } catch {
+      } catch (error) {
+        console.log(error);
         return false;
       }
     },
